c02f2bf7d8af90f32592bf18b9b919971993ca1d
danielpoehle <d.poehle@gmx.de>
2018-06-09 12:01:54 +0200

    file reader for text

--- controllers.js
+++ controllers.js
@@ -4,9 +4,36 @@
    myApp.controller('FileUploadController', function ($scope) {
 
        $scope.uploadFile = function () {
-           var file = $scope.myFile;
-           console.log(file);
+           var fileList = $scope.myFile;
+
+           // Check for the various File API support.
+           if (window.File && window.FileReader && window.FileList && window.Blob) {
+             // Great success! All the File APIs are supported.
+           } else {
+             alert('The File APIs are not fully supported in this browser.');
+           }
+
+           for (var i = 0, len = fileList.length; i < len; i++) {
+             readAndConvertFile(fileList[i]);
+           }
        };
    });
+
+   function readAndConvertFile(file) {
+     //console.log(file);
+     var textType = /text.*/;
+     if (file.type.match(textType)) {
+       var reader = new FileReader();
+
+       reader.onload = function(e) {
+         console.log(reader.result);
+       };
+
+       reader.readAsText(file);
+
+     } else {
+       console.log("File not supported!");
+     }
+   }
 
 })();
