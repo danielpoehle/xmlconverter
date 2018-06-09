f08b0352052733af7400ae28510a017d4fd9c780
danielpoehle <d.poehle@gmx.de>
2018-06-09 10:48:27 +0200

    file binding established

--- controllers.js
+++ controllers.js
@@ -1,1 +1,12 @@
- 
+(function () {
+   'use strict';
+   var myApp = angular.module('app');
+   myApp.controller('FileUploadController', function ($scope) {
+
+       $scope.uploadFile = function () {
+           var file = $scope.myFile;
+           console.log(file);
+       };
+   });
+
+})();
